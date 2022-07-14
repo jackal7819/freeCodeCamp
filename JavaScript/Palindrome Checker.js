59a8e19a6e6d5ab056d0db6f72bc385a31431cc2
jackal7819 <jackal3166@gmail.com>
2022-07-14 23:34:16 +0300

    Create Palindrome Checker.js

---
+++ JavaScript/Palindrome Checker.js
@@ -0,0 +1,5 @@
+function palindrome(str) {
+    const strAlphanumeric = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    const strReverse = strAlphanumeric.split('').reverse().join('');
+    return strAlphanumeric === strReverse;
+  }
