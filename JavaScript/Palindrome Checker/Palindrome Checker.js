39d9b9917d4ecfdf914044214e0526c74b2ae375
jackal7819 <106143167+jackal7819@users.noreply.github.com>
2022-07-14 23:38:51 +0300

    Update Palindrome Checker.js

--- JavaScript/Palindrome Checker/Palindrome Checker.js
+++ JavaScript/Palindrome Checker/Palindrome Checker.js
@@ -2,4 +2,4 @@ function palindrome(str) {
     const strAlphanumeric = str.toLowerCase().replace(/[^a-z0-9]/g, '');
     const strReverse = strAlphanumeric.split('').reverse().join('');
     return strAlphanumeric === strReverse;
-  }
+}
