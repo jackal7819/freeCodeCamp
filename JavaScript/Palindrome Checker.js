9ade588bfbaf012b6aed315f74d08ff06007ce1a
jackal7819 <jackal3166@gmail.com>
2022-07-14 23:35:48 +0300

    Delete Palindrome Checker.js

--- JavaScript/Palindrome Checker.js
+++
@@ -1,5 +0,0 @@
-function palindrome(str) {
-    const strAlphanumeric = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-    const strReverse = strAlphanumeric.split('').reverse().join('');
-    return strAlphanumeric === strReverse;
-  }
