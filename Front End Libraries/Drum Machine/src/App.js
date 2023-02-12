5dfa8474326f6889f2b7d8922df721aa25d0348e
“jackal7819” <jackal3166@gmail.com>
2023-02-12 17:44:35 +0200

    Create Drum Machine

---
+++ Front End Libraries/Drum Machine/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Drum from './components/Drum';
+
+const App = () => {
+    return (
+        <div className='app'>
+            <Drum />
+        </div>
+    );
+};
+
+export default App;
